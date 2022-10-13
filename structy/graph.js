b3a84f6a2ef6ee9d8765ecf354cd4768fa7254c6
Evie <evie.zengyiying@gmail.com>
2022-10-12 21:15:44 -0700

    add 3 graph problems

--- structy/graph.js
+++ structy/graph.js
@@ -10,9 +10,9 @@
             // ['O', 'X', 'X', 'O', 'O'],
             // ['C', 'O', 'O', 'O', 'O'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 1, 2); // -> 4
-        
+
     // test_01:
         // const grid = [
             // ['O', 'O', 'O', 'O', 'O'],
@@ -22,9 +22,9 @@
             // ['O', 'X', 'X', 'O', 'O'],
             // ['C', 'O', 'O', 'O', 'O'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 0, 0); // -> 5
-        
+
     // test_02:
         // const grid = [
             // ['O', 'O', 'X', 'X', 'X'],
@@ -36,7 +36,7 @@
             // ['O', 'O', 'C', 'O', 'O'],
             // ['O', 'O', 'O', 'O', 'O'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 3, 4); // -> 9
         // test_03:
         // const grid = [
@@ -44,18 +44,18 @@
             // ['O', 'X', 'X', 'X', 'O'],
             // ['O', 'X', 'C', 'C', 'O'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 1, 4); // -> 2
-        
+
     // test_04:
         // const grid = [
             // ['O', 'O', 'X', 'O', 'O'],
             // ['O', 'X', 'X', 'X', 'O'],
             // ['O', 'X', 'C', 'C', 'O'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 2, 0); // -> -1
-        
+
     // test_05:
         // const grid = [
             // ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
@@ -71,8 +71,209 @@
             // ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'X'],
             // ['O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'X', 'C'],
         // ];
-        // 
+        //
         // closestCarrot(grid, 0, 0); // -> -1
 
 
+
+
+
+// longest path
+    // Write a function, longestPath, that takes in an adjacency list for a directed acyclic graph.The function should return the length of the longest path within the graph.A path may start and end at any two nodes.The length of a path is considered the number of edges in the path, not the number of nodes.
+
+    // test_00:
+    //     const graph = {
+    //         a: ['c', 'b'],
+    //         b: ['c'],
+    //         c: []
+    //     };
+
+    //     longestPath(graph); // -> 2
+
+    // test_01:
+    //     const graph = {
+    //         a: ['c', 'b'],
+    //         b: ['c'],
+    //         c: [],
+    //         q: ['r'],
+    //         r: ['s', 'u', 't'],
+    //         s: ['t'],
+    //         t: ['u'],
+    //         u: []
+    //     };
+
+    //     longestPath(graph); // -> 4
+
+    // test_02:
+    //     const graph = {
+    //         h: ['i', 'j', 'k'],
+    //         g: ['h'],
+    //         i: [],
+    //         j: [],
+    //         k: [],
+    //         x: ['y'],
+    //         y: []
+    //     };
+
+    //     longestPath(graph); // -> 2
+
+    // test_03:
+    //     const graph = {
+    //         a: ['b'],
+    //         b: ['c'],
+    //         c: [],
+    //         e: ['f'],
+    //         f: ['g'],
+    //         g: ['h'],
+    //         h: []
+    //     };
+
+    //     longestPath(graph); // -> 3
+
+
+
+
+// semesters required
+    // Write a function, semestersRequired, that takes in a number of courses(n) and a list of prerequisites as arguments.Courses have ids ranging from 0 through n - 1. A single prerequisite of[A, B] means that course A must be taken before course B.Return the minimum number of semesters required to complete all n courses.There is no limit on how many courses you can take in a single semester, as long the prerequisites of a course are satisfied before taking it.
+    // Note that given prerequisite[A, B], you cannot take course A and course B concurrently in the same semester.You must take A in some semester before B.
+    // You can assume that it is possible to eventually complete all courses.
+
+    // test_00:
+        // const numCourses = 6;
+        // const prereqs = [
+            // [1, 2],
+            // [2, 4],
+            // [3, 5],
+            // [0, 5],
+        // ];
+        // semestersRequired(numCourses, prereqs); // -> 3
+
+    // test_01:
+        // const numCourses = 7;
+        // const prereqs = [
+            // [4, 3],
+            // [3, 2],
+            // [2, 1],
+            // [1, 0],
+            // [5, 2],
+            // [5, 6],
+        // ];
+        // semestersRequired(numCourses, prereqs); // -> 5
+
+    // test_02:
+        // const numCourses = 5;
+        // const prereqs = [
+            // [1, 0],
+            // [3, 4],
+            // [1, 2],
+            // [3, 2],
+        // ];
+        // semestersRequired(numCourses, prereqs); // -> 2
+
+    // test_03:
+        // const numCourses = 12;
+        // const prereqs = [];
+        // semestersRequired(numCourses, prereqs); // -> 1
+
+    // test_04:
+        // const numCourses = 3;
+        // const prereqs = [
+            // [0, 2],
+            // [0, 1],
+            // [1, 2],
+        // ];
+        // semestersRequired(numCourses, prereqs); // -> 3
+
+    // test_05:
+        // const numCourses = 6;
+        // const prereqs = [
+            // [3, 4],
+            // [3, 0],
+            // [3, 1],
+            // [3, 2],
+            // [3, 5],
+        // ];
+        // semestersRequired(numCourses, prereqs); // -> 2
+
+
+
+
+
+// best bridge
+
+    // Write a function, bestBridge, that takes in a grid as an argument.The grid contains water(W) and land(L).There are exactly two islands in the grid.An island is a vertically or horizontally connected region of land.Return the minimum length bridge needed to connect the two islands.A bridge does not need to form a straight line.
+
+    // test_00:
+        // const grid = [
+            // ["W", "W", "W", "L", "L"],
+            // ["L", "L", "W", "W", "L"],
+            // ["L", "L", "L", "W", "L"],
+            // ["W", "L", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W"],
+        // ];
+        // bestBridge(grid); // -> 1
         
+    // test_01:
+        // const grid = [
+            // ["W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W"],
+            // ["L", "L", "W", "W", "L"],
+            // ["W", "L", "W", "W", "L"],
+            // ["W", "W", "W", "L", "L"],
+            // ["W", "W", "W", "W", "W"],
+        // ];
+        // bestBridge(grid); // -> 2
+        
+    // test_02:
+        // const grid = [
+            // ["W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "L", "W"],
+            // ["L", "W", "W", "W", "W"],
+        // ];
+        // bestBridge(grid); // -> 3
+        
+    // test_03:
+        // const grid = [
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "L", "W", "W"],
+            // ["W", "W", "W", "W", "L", "L", "W", "W"],
+            // ["W", "W", "W", "W", "L", "L", "L", "W"],
+            // ["W", "W", "W", "W", "W", "L", "L", "L"],
+            // ["L", "W", "W", "W", "W", "L", "L", "L"],
+            // ["L", "L", "L", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+        // ];
+        // bestBridge(grid); // -> 3
+        
+    // test_04:
+        // const grid = [
+            // ["L", "L", "L", "L", "L", "L", "L", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "L", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "W", "W", "W", "W", "W", "W", "L"],
+            // ["L", "L", "L", "L", "L", "L", "L", "L"],
+        // ];
+        // bestBridge(grid); // -> 2
+        
+    // test_05:
+        // const grid = [
+            // ["W", "L", "W", "W", "W", "W", "W", "W"],
+            // ["W", "L", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "W", "W"],
+            // ["W", "W", "W", "W", "W", "W", "L", "W"],
+            // ["W", "W", "W", "W", "W", "W", "L", "L"],
+            // ["W", "W", "W", "W", "W", "W", "W", "L"],
+        // ];
+        // bestBridge(grid); // -> 8
--- structy/graph_solution.js
+++ structy/graph_solution.js
@@ -32,3 +32,204 @@ const closestCarrot = (grid, startRow, startCol) => {
     return -1;
 };
 
+
+
+
+// longest path
+
+// way 1 (depth first) 
+// e = # edges
+// n = # nodes
+// Time: O(e)
+// Space: O(n)
+const longestPath = (graph) => {
+    const distance = {};
+    for (let node in graph) {
+        if (graph[node].length === 0) {
+            distance[node] = 0;
+        }
+    }
+
+    for (let node in graph) {
+        traverseDistance(graph, node, distance);
+    }
+
+    return Math.max(...Object.values(distance));
+};
+
+const traverseDistance = (graph, node, distance) => {
+    if (node in distance) return distance[node];
+
+    let maxDistance = 0;
+    for (let neighbor of graph[node]) {
+        const attempt = traverseDistance(graph, neighbor, distance);
+        if (attempt > maxDistance) maxDistance = attempt;
+    }
+
+    distance[node] = 1 + maxDistance;
+    return distance[node];
+};
+
+
+
+
+// semesters required
+
+// way 1 (depth first)
+// p = # prereqs
+// c = # courses
+// Time: O(p)
+// Space: O(c)
+// strategy:
+// 1. build the graph  2. eage case(if the len node arr equal to 0, distance = 0/1)  3.traverseDistance(recursive)
+const semestersRequired = (numCourses, prereqs) => {
+    const graph = buildGraph(numCourses, prereqs);
+    const distance = {};
+
+    for (let i = 0; i < numCourses; i += 1) {
+        if (graph[i].length === 0) distance[i] = 1;
+    }
+
+    for (let i = 0; i < numCourses; i += 1) {
+        traverseDistance2(graph, i, distance)
+    }
+
+    return Math.max(...Object.values(distance));
+};
+
+const traverseDistance2 = (graph, node, distance) => {
+    if (node in distance) return distance[node];
+
+    let maxDistance = 0;
+    for (let neighbor of graph[node]) {
+        const neighborDistance = traverseDistance2(graph, neighbor, distance);
+        if (neighborDistance > maxDistance) maxDistance = neighborDistance;
+    }
+
+    distance[node] = maxDistance + 1;
+    return distance[node];
+};
+
+const buildGraph = (numCourses, prereqs) => {
+    const graph = {};
+
+    for (let i = 0; i < numCourses; i += 1) {
+        graph[i] = [];
+    }
+
+    for (let prereq of prereqs) {
+        const [a, b] = prereq;
+        graph[a].push(b);
+    }
+
+    return graph;
+};
+
+// way 2
+// const semestersRequired = (numCourses, prereqs) => {
+//     const graph = buildGraph(numCourses, prereqs);
+
+//     let distance = {};
+//     for (let course in graph) {
+//         if (graph[course].length === 0) distance[course] = 1;
+//     }
+
+//     for (let course in graph) {
+//         traverseDistance(graph, course, distance)
+//     }
+//     return Math.max(...Object.values(distance))
+// };
+
+// const traverseDistance = (graph, node, distance) => {
+//     let max = 0;
+//     for (let neighbor of graph[node]) {
+//         let attampt = traverseDistance(graph, neighbor, distance)
+//         if (attampt > max) max = attampt;
+//     }
+//     distance[node] = 1 + max;
+//     return distance[node];
+// }
+
+// const buildGraph = (numCourses, prereqs) => {
+//     let graph = {};
+//     for (let i = 0; i < numCourses; i++) {
+//         graph[i] = [];
+//     }
+
+//     for (let prereq of prereqs) {
+//         const [a, b] = prereq;
+//         graph[a].push(b);
+//     }
+//     return graph;
+// }
+
+
+
+
+// best bridge
+
+// way 1
+// r = number of rows
+// c = number of columns
+// Time: O(rc)
+// Space: O(rc)
+const bestBridge = (grid) => {
+    let mainIsland;
+    for (let r = 0; r < grid.length; r += 1) {
+        for (let c = 0; c < grid[0].length; c += 1) {
+            const possibleIsland = traverseIsland(grid, r, c, new Set());
+            if (possibleIsland.size > 0) {
+                mainIsland = possibleIsland;
+                break;
+            }
+        }
+    }
+
+    const visited = new Set(mainIsland);
+    const queue = [];
+    for (let pos of mainIsland) {
+        const [row, col] = pos.split(',').map(Number);
+        queue.push([row, col, 0]);
+    }
+
+    while (queue.length > 0) {
+        const [row, col, distance] = queue.shift();
+
+        const pos = row + ',' + col;
+        if (grid[row][col] === 'L' && !mainIsland.has(pos)) return distance - 1;
+
+        const deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+        for (let delta of deltas) {
+            const [deltaRow, deltaCol] = delta;
+            const neighborRow = row + deltaRow;
+            const neighborCol = col + deltaCol;
+            const neighborPos = neighborRow + ',' + neighborCol;
+            if (isInbounds(grid, neighborRow, neighborCol) && !visited.has(neighborPos)) {
+                visited.add(neighborPos);
+                queue.push([neighborRow, neighborCol, distance + 1]);
+            }
+        }
+    }
+};
+
+const isInbounds = (grid, row, col) => {
+    const rowInbounds = 0 <= row && row < grid.length;
+    const colInbounds = 0 <= col && col < grid[0].length;
+    return rowInbounds && colInbounds;
+};
+
+const traverseIsland = (grid, row, col, visited) => {
+    if (!isInbounds(grid, row, col) || grid[row][col] === 'W') return visited;
+
+    const pos = row + ',' + col;
+    if (visited.has(pos)) return visited;
+
+    visited.add(pos);
+
+    traverseIsland(grid, row - 1, col, visited);
+    traverseIsland(grid, row + 1, col, visited);
+    traverseIsland(grid, row, col - 1, visited);
+    traverseIsland(grid, row, col + 1, visited);
+
+    return visited;
+};
