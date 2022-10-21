8f0455b3a21b10dcc427278dfc8b273e8504e82d
Evie <evie.zengyiying@gmail.com>
2022-10-20 21:14:33 -0700

    add 2 mix problems

--- structy/mix_recall.js
+++ structy/mix_recall.js
@@ -145,3 +145,169 @@
         
         // q
         // middleValue(q); // q
+
+
+
+
+// linked list cycle
+
+    // Write a function, linkedListCycle, that takes in the head of a linked list as an argument.The function should return a boolean indicating whether or not the linked list contains a cycle.
+
+    // test_00:
+        // const a = new Node('a');
+        // const b = new Node('b');
+        // const c = new Node('c');
+        // const d = new Node('d');
+
+        // a.next = b;
+        // b.next = c;
+        // c.next = d;
+        // d.next = b; // cycle
+
+        //         _______
+        //       /        \
+        // a -> b -> c -> d 
+
+        // linkedListCycle(a);  // true
+        
+    // test_01:
+        // const q = new Node('q');
+        // const r = new Node('r');
+        // const s = new Node('s');
+        // const t = new Node('t');
+        // const u = new Node('u');
+
+        // q.next = r;
+        // r.next = s;
+        // s.next = t;
+        // t.next = u;
+        // u.next = q; // cycle
+
+        //    ________________
+        //  /                 \
+        // q -> r -> s -> t -> u 
+
+        // linkedListCycle(q);  // true
+        
+    // test_02:
+        // const a = new Node('a');
+        // const b = new Node('b');
+        // const c = new Node('c');
+        // const d = new Node('d');
+
+        // a.next = b;
+        // b.next = c;
+        // c.next = d;
+
+        // a -> b -> c -> d 
+
+        // linkedListCycle(a);  // false
+        
+    // test_03:
+        // const q = new Node('q');
+        // const r = new Node('r');
+        // const s = new Node('s');
+        // const t = new Node('t');
+        // const u = new Node('u');
+
+        // q.next = r;
+        // r.next = s;
+        // s.next = t;
+        // t.next = u;
+        // u.next = t; // cycle
+
+        //                   __
+        //                 /   \
+        // q -> r -> s -> t -> u 
+
+        // linkedListCycle(q);  // true
+        
+    // test_04:
+        // const p = new Node('p');
+
+        // p
+
+        // linkedListCycle(p); // false
+        
+    // test_05:
+        // linkedListCycle(null); // false
+
+
+
+
+// lowest common ancestor
+
+    // Write a function, lowestCommonAncestor, that takes in the root of a binary tree and two values.The function should return the value of the lowest common ancestor of the two values in the tree.
+    // You may assume that the tree values are unique and the tree is non - empty.
+    // Note that a node may be considered an ancestor of itself.
+
+        // example tree
+        // const a = new Node('a');
+        // const b = new Node('b');
+        // const c = new Node('c');
+        // const d = new Node('d');
+        // const e = new Node('e');
+        // const f = new Node('f');
+        // const g = new Node('g');
+        // const h = new Node('h');
+
+        // a.left = b;
+        // a.right = c;
+        // b.left = d;
+        // b.right = e;
+        // c.right = f;
+        // e.left = g;
+        // e.right = h;
+
+        //      a
+        //    /    \
+        //   b      c
+        //  / \      \
+        // d   e      f
+        //    / \
+        //    g  h
+        // test_00
+        // lowestCommonAncestor(a, 'd', 'h'); // b
+        // test_01
+        // lowestCommonAncestor(a, 'd', 'g'); // b
+        // test_02
+        // lowestCommonAncestor(a, 'g', 'c'); // a
+        // test_03
+        // lowestCommonAncestor(a, 'b', 'g'); // b
+        // test_04
+        // lowestCommonAncestor(a, 'f', 'c'); // c
+        // example tree
+        // const l = new Node('l');
+        // const m = new Node('m');
+        // const n = new Node('n');
+        // const o = new Node('o');
+        // const p = new Node('p');
+        // const q = new Node('q');
+        // const r = new Node('r');
+        // const s = new Node('s');
+        // const t = new Node('t');
+
+        // l.left = m;
+        // l.right = n;
+        // n.left = o;
+        // n.right = p;
+        // o.left = q;
+        // o.right = r;
+        // p.left = s;
+        // p.right = t;
+
+        //        l
+        //     /     \
+        //    m       n
+        //         /    \
+        //         o     p
+        //        / \   / \
+        //       q   r s   t
+        // test_05
+        // lowestCommonAncestor(l, 'r', 'p'); // n
+        // test_06
+        // lowestCommonAncestor(l, 'm', 'o'); // l
+        // test_07
+        // lowestCommonAncestor(l, 't', 'q'); // n
+        // test_08
+        // lowestCommonAncestor(l, 's', 'p'); // p
--- structy/mix_recall_solution.js
+++ structy/mix_recall_solution.js
@@ -52,3 +52,85 @@ const middleValue = (head) => {
 
 //     return slow.val;
 // };
+
+
+
+
+// linked list cycle
+
+// way 1 (using a set)
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+const linkedListCycle = (head) => {
+    const nodes = new Set();
+
+    let current = head;
+    while (current !== null) {
+        if (nodes.has(current)) return true;
+        nodes.add(current);
+        current = current.next;
+    }
+
+    return false;
+};
+
+// way 2 (using two pointers)
+// n = number of nodes
+// Time: O(n)
+// Space: O(1)
+
+// const linkedListCycle = (head) => {
+//     let slow = head;
+//     let fast = head;
+//     let firstIteration = true;
+//     while (fast !== null && fast.next !== null) {
+//         if (slow === fast && !firstIteration) return true;
+
+//         slow = slow.next;
+//         fast = fast.next.next;
+//         firstIteration = false;
+//     }
+
+//     return false;
+// };
+
+
+
+
+
+// lowest common ancestor
+
+// depth first path
+// n = number of nodes
+// Time: O(n)
+// Space: O(n)
+const lowestCommonAncestor = (root, val1, val2) => {
+    const path1 = findPath(root, val1);
+    const path2 = findPath(root, val2);
+    const set2 = new Set(path2);
+
+    for (let val of path1) {
+        if (set2.has(val)) return val;
+    }
+};
+
+const findPath = (root, targetVal) => {
+    if (root === null) return null;
+
+    if (root.val === targetVal) return [root.val];
+
+    const leftPath = findPath(root.left, targetVal);
+    if (leftPath !== null) {
+        leftPath.push(root.val);
+        return leftPath;
+    }
+
+    const rightPath = findPath(root.right, targetVal);
+    if (rightPath !== null) {
+        rightPath.push(root.val);
+        return rightPath;
+    }
+
+    return null;
+};
